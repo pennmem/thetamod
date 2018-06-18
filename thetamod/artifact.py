db38ee6a6b5a772855290255e872d5706c1c9faf
leond <lad258@cornell.edu>
2018-06-18 17:35:12 -0400

    added module for eliminating artifactual signal, along with some test cases

---
+++ thetamod/artifact.py
@@ -0,0 +1,94 @@
+import numpy as np
+from cmlreaders import CMLReader
+from scipy.stats import ttest_rel
+from scipy.stats import levene
+import itertools
+
+"""
+Three-stage filter for rejecting artifactual signals:
+
+1. Reject any channel labelled as bad in `electrode_categories.txt`
+2. Reject any trial that shows amplifier saturation
+3. Reject any channel that shows too large a difference between the power spectra
+pre- and post-stim, as measured using a paired t-test
+
+"""
+
+
+def get_bad_channel_names(subject, montage, just_bad=None,rhino_root='/'):
+
+    reader = CMLReader(subject, montage=montage,rootdir=rhino_root)
+    electrode_categories = reader.load('electrode_categories')
+    if just_bad is True:
+        return (electrode_categories.get('bad_electrodes',[])
+                + electrode_categories.get('broken_leads',[])
+                )
+    else:
+        return list(itertools.chain(*electrode_categories.values()))
+
+
+def get_saturated_events_mask(eegs):
+    # Return array of chans x events with 1s where saturation is found
+
+    def zero_runs(a):
+        a = np.array(a)
+        # Create an array that is 1 where a is 0, and pad each end with an extra 0.
+        iszero = np.concatenate(([0], np.equal(a, 0).view(np.int8), [0]))
+        absdiff = np.abs(np.diff(iszero))
+        # Runs start and end where absdiff is 1.
+        ranges = np.where(absdiff == 1)[0].reshape(-1, 2)
+        return ranges
+
+    sat_events = np.zeros([eegs.shape[0], eegs.shape[1]])
+
+    for i in range(eegs.shape[0]):
+        for j in range(eegs.shape[1]):
+            ts = eegs[i, j]
+            zr = zero_runs(np.diff(np.array(ts)))
+            numzeros = zr[:, 1] - zr[:, 0]
+            if (numzeros > 9).any():
+                sat_events[i, j] = 1
+                continue
+
+    return sat_events.astype(bool)
+
+
+def get_channel_exclusion_mask(pre_eeg, post_eeg):
+    """
+    Estimate which channels show broadband DC shift post-stimulation
+    using T-test and Levene variance test.
+
+    Parameters
+    ----------
+    pre_eeg: np.ndarray
+       Pre-stimulus EEG signals
+    post_eeg
+       Post-stimulus EEG signals
+
+    Returns
+    -------
+
+    pvals: np.ndarray
+        P-values from paired t-test between pre-stim EEG and post-stim EEG
+
+    lev_pvals: np.ndarray
+        P-values from Levene variance test between pre-stim EEG and post-stim
+        EEG.
+    """
+
+    def justfinites(arr):
+        return arr[np.isfinite(arr)]
+
+    pvals = []
+    lev_pvals = []
+    assert pre_eeg.shape == post_eeg.shape #FIXME: RAISE A PROPER EXCEPTION
+    for i in range(pre_eeg.shape[0]):
+        eeg_t_chan, eeg_p_chan = ttest_rel(post_eeg, pre_eeg, nan_policy='omit')
+        pvals.append(eeg_p_chan)
+        try:
+            lev_t, lev_p = levene(justfinites(post_eeg), justfinites(pre_eeg))
+            lev_pvals.append(lev_p)
+        except Exception:
+            lev_pvals.append(0.0)
+
+    return np.array(pvals), np.array(lev_pvals)
---
+++ thetamod/test/test_artifact.py
@@ -0,0 +1,52 @@
+import pytest
+import ethan.MNE_pipeline_refactored
+import ethan.pred_stim_pipeline
+import thetamod.artifact
+from cmlreaders import CMLReader
+from thetamod.tmi import get_stim_events,get_eeg
+
+
+@pytest.mark.parametrize("just_bad",[True, False])
+@pytest.mark.parametrize("subject, montage", [
+    ('R1111M', 0),
+])
+def test_electrode_categories(subject, montage, just_bad, rhino_root):
+    ethan_electrode_list = ethan.MNE_pipeline_refactored.exclude_bad(
+        subject, montage, just_bad, rhino_root)
+    ethan_electrode_list = [name for name in ethan_electrode_list if
+                            name.upper()==name and name.isalnum()
+                            and 'R1' not in name]
+    new_electrode_list = thetamod.artifact.get_bad_channel_names(
+        subject, montage, just_bad, rhino_root
+    )
+    assert len(ethan_electrode_list) == len(new_electrode_list)
+    assert set(ethan_electrode_list) == set(new_electrode_list)
+
+
+@pytest.mark.parametrize("subject, experiment", [
+    ('R1286J', 'catFR3',),
+    ('R1332M', 'PS4_catFR'),
+])
+def test_saturated_events(subject, experiment, rhino_root):
+    reader = CMLReader(subject, experiment, session=0, rootdir=rhino_root)
+    events = get_stim_events(reader)
+    eeg = get_eeg('pre', reader, events).data
+    ethan_artifact_mask = ethan.pred_stim_pipeline.find_sat_events(eeg)
+    new_artifact_mask = thetamod.artifact.get_saturated_events_mask(eeg)
+    assert new_artifact_mask.any()
+    assert (ethan_artifact_mask == new_artifact_mask).all()
+
+@pytest.mark.skip
+def test_channel_exclusion_mask():
+    full_eeg = get_eeg()
+    ethan_p, ethan_lev_p = ethan.pred_stim_pipeline.channel_exclusion(full_eeg)
+    pre_eeg, post_eeg = (get_eeg(which,...) for which in ('pre', 'post'))
+    new_p, new_levp = thetamod.artifact.get_channel_exclusion_mask(
+        pre_eeg,post_eeg
+    )
+    assert (new_p == ethan_p).all()
+    assert (new_levp == ethan_lev_p).all()
+
+
+if __name__ == "__main__":
+    test_electrode_categories('R1111M', 0, False, '/Volumes/rhino_root')
